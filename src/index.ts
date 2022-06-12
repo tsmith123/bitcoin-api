cd17e69cf48218d62d2a45e31aa353bb3cc25774
Thomas Smith <tomdsmith1@gmail.com>
2022-06-12 08:31:43 +0100

    Add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-const express = require('express')
+import express from 'express'
+import { Request, Response } from 'express';
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
